e1764b5e1bae2baffb6edb12060204f90827c1ac
mkeehley <224448428+mkeehley@users.noreply.github.com>
2026-08-19 20:13:17 -0400

    Implement slideshow functionality with auto-rotate

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
-const cards = document.querySelectorAll(".card-content");
+const slides = document.querySelectorAll(".project-slide");
+const dots = document.querySelectorAll(".dot");
 
-let currentCard = 0;
+const nextButton = document.getElementById("nextProject");
+const previousButton = document.getElementById("previousProject");
 
-setInterval(() => {
-    cards[currentCard].classList.remove("active");
+let currentSlide = 0;
+let autoRotate;
 
-    currentCard = (currentCard + 1) % cards.length;
 
-    cards[currentCard].classList.add("active");
-}, 3000);
+function showSlide(index) {
+
+    slides[currentSlide].classList.remove("active");
+    dots[currentSlide].classList.remove("active");
+
+    currentSlide = (index + slides.length) % slides.length;
+
+    slides[currentSlide].classList.add("active");
+    dots[currentSlide].classList.add("active");
+}
+
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+
+function previousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+
+function startAutoRotate() {
+
+    autoRotate = setInterval(() => {
+        nextSlide();
+    }, 5000);
+
+}
+
+
+function resetAutoRotate() {
+
+    clearInterval(autoRotate);
+    startAutoRotate();
+
+}
+
+
+nextButton.addEventListener("click", () => {
+    nextSlide();
+    resetAutoRotate();
+});
+
+
+previousButton.addEventListener("click", () => {
+    previousSlide();
+    resetAutoRotate();
+});
+
+
+dots.forEach((dot, index) => {
+
+    dot.addEventListener("click", () => {
+        showSlide(index);
+        resetAutoRotate();
+    });
+
+});
+
+
+startAutoRotate();
